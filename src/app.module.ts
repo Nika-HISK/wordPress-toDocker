c3eceeb5dec0733bd0c4c3c7f92bb27df65c7781
nika.chinchaladze <chinchaladzebeso4@gmail.com>
2024-10-18 16:27:03 +0400

    are you winning son?

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { WpModule } from './wp/wp.module';
 import { DockerModule } from './docker/docker.module';
+import { WordpressModule } from './wp/wp.module';
 
 @Module({
-  imports: [WpModule, DockerModule],
+  imports: [WordpressModule, DockerModule],
   controllers: [AppController],
   providers: [AppService],
 })
