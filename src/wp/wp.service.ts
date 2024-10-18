b8b7c010dba20c0457e236d631bc593d05327026
sabaabramishvili <sabramashvili02@gmail.com>
2024-10-18 19:25:29 +0400

    small changes

--- src/wp/wp.service.ts
+++ src/wp/wp.service.ts
@@ -57,7 +57,7 @@ volumes:
 
       // Step 3: Wait for the services to be ready
       console.log('Waiting for services to be ready...');
-      await this.sleep(2000); // Adjust sleep time if necessary
+      await this.sleep(1000); // Adjust sleep time if necessary
 
       // Step 4: Get Docker container name
       console.log('Retrieving WordPress container name...');
@@ -76,7 +76,7 @@ volumes:
       await execAsync(`docker exec ${wordpressContainerName} chmod +x wp-cli.phar`);
       await execAsync(`docker exec ${wordpressContainerName} mv wp-cli.phar /usr/local/bin/wp`);
       console.log('WP-CLI installed.');
-      await this.sleep(55000);
+      await this.sleep(1000);
       // Step 7: Check if wp-config.php exists
       console.log('Checking for existing wp-config.php...');
       const checkConfigCmd = `docker exec ${wordpressContainerName} ls /var/www/html/wp-config.php`;
@@ -88,7 +88,7 @@ volumes:
       } catch (error) {
         console.log('wp-config.php does not exist. No need to remove.');
       }
-      await this.sleep(20000);
+      await this.sleep(6000);
       // Step 8: Generate new wp-config.php using WP-CLI
       console.log('Creating new wp-config.php...');
       await execAsync(`docker exec ${wordpressContainerName} wp config create --dbname=ramedb --dbuser=nika_chinchaladze --dbpass=paroli123 --dbhost=db --path=/var/www/html --allow-root`);
@@ -97,11 +97,11 @@ volumes:
       // Step 9: Install WordPress
       console.log('Installing WordPress...');
       await execAsync(`docker exec ${wordpressContainerName} wp core install --url="http://localhost:8000" --title="Your Site Title" --admin_user="admin" --admin_password="adminpass" --admin_email="admin@example.com" --skip-email --allow-root`);
-      await this.sleep(15000);
+      await this.sleep(1000);
       // Step 10: Install and activate language
       console.log('Installing and activating language...');
       await execAsync(`docker exec ${wordpressContainerName} wp language core install en_US --activate --allow-root`);
-      await this.sleep(2000);
+      await this.sleep(1000);
       console.log('WordPress setup complete!');
       return 'WordPress setup complete!';
       
